525c121ea83803bc7458880ad4b3472c38c806df
MirsaidovMirziyod <mirsaidovmirziyod1507@gmail.com>
2023-02-23 21:52:17 +0500

    for sintacsis

---
+++ main.js
@@ -0,0 +1,222 @@
+// if(score > 0 && score % 2 !=0 ){
+//     console.log('toq musbat');
+// }
+// else if(score < 0 && score % 2 !=0){
+//     console.log('toq manfiy');
+// }
+// else if(score > 0 && score % 2 ==0){
+//     console.log('juft musbat');
+// }
+// else{
+//     console.log('juft manfiy');
+// }
+
+// 20-masala
+// let year = 2007;
+
+// let have = 2023;
+// if(have -= year){
+//     console.log(have , 'yosh');
+// }
+// else{
+//     console.log('xato');
+// }
+
+// 21-masala
+// let a = 3;
+// let b = -3;
+// let c = -5;
+// if(a%2 == 0  b%2 == 0  c%2 == 0){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// 22-masala
+// let a = 66;
+// let b = 55;
+
+// if(a %2 !=0){
+//     console.log("toq son");
+// }
+// else{
+//     console.log("juft son");
+// }
+
+// 23-masala
+// let a = -6;
+// let b = -4;
+// let c = -2;
+
+// if(a > 0  b > 0  c > 0){
+//     console.log('musbat son');
+// }
+// else{
+//     console.log("manfiy son");
+// }
+
+// 24-masala
+// let a = -6;
+// let b = 4;
+// let c = -2;
+
+// if(a> 0){
+//     console.log('musbat son', a);
+// }
+// else if (b>0){
+//     console.log("musbat son ", b);
+// }
+// else if (c>0){
+//     console.log('musbat son', c);
+// }
+// else{
+//     console.log('manfiy son');
+// }
+
+// 25-masala
+
+///////////////////////////////////////////////////////////
+// var number = 551;
+// var result = true;
+// var bir = 551%10;
+// var on = 551/10%10;
+// var yuz = 551/100%10;
+// if((yuz == bir  yuz == on  on == bir) || (yuz== onlik && onlik ==bir && yuz == bir)){
+//   console.log(result = false);
+// }
+//   console.log(number);
+
+// 26-masala
+// let n = null;
+// let a,b,c;
+// c = n%10;
+// n /= 10;
+// b = n%10;
+// n /= 10;
+// a = n;
+// if(a<b && b<c){
+//   console.log(true);
+// }
+// else{
+//   console.log(false);
+// }
+
+// 27-masala
+// let sum = 'hp';
+// let asus = 'asus';
+// let acer = 'acer';
+// let apple = 'apple';
+// let dol = 'doll';
+// let hp = 'hp';
+// if(sum == asus){
+//   console.log("6.000.000");
+// }
+// else if (sum == acer){
+//   console.log("5.000.000 ");
+// }
+// else if (sum == apple){
+//   console.log("15.000.000 ");
+// }
+// else if (sum == dol){
+//   console.log("4.000.000 ");
+// }
+// else if (sum == hp){
+//   console.log("8.000.000 ");
+// }
+// else{
+//   console.log("nimala buvoti oka malikaga tushin undan kora 🤣");
+// }
+
+// 28-masala
+// let k = 10;
+// for(i = 1; i < k; i++){
+//     if(i%2 !=0){
+//         console.log(i,"tub son");
+//     }
+//     else{
+//         console.log(i,"murakkab son");
+//     }
+// }
+
+// FOR masalalar
+
+// 1-masala
+// let k = 5;
+
+// let n = 2;
+
+// for(let i=0; i<n; i++){
+//   console.log(k);
+// }
+
+// 2-masala
+// let a = 5;
+// let b = 2;
+
+// for(let i=b; i<=a; i++){
+//   if(a>b){
+//     console.log(i);
+//   }
+//   else{
+//     console.log(false);
+//   }
+// }
+
+// 3-masala
+// let a = 8;
+// let b = 1;
+
+// for (let i = a-1; i > b; i--) {
+//   console.log(i);
+// }
+
+// 4-masala
+// let a = 1;
+// for(let i=1; i<=10; i++){
+//     console.log(i,'kg konfet',i*10000,"so'm");
+// }
+
+// 5-masala
+// let a = 1;
+// for(let i=0.1,j = 1; i<a; j++){
+//   i = j/10
+//   console.log(i,'kg konfet',j,"000","so'm");
+//   }
+
+// 6-masala
+// for (let i = 12; i <= 20;i+=2 ){
+//   console.log(i / 10, "kg -> ", 10000 * i/10, "so'm");
+// }
+
+//7-masala
+// let sum = 0;
+// for(let i=1; i<=10;i++){
+//     sum+=i;
+//     console.log(sum);
+// }
+
+// 8-masala
+// let sum = 1;
+// for(let i=1; i<=10;i++){
+//     sum*=i;
+//   }
+//   console.log(sum);
+
+// 9-masala
+// let sum=0;
+
+// for(let i=1;i<=10;i++){
+//   sum+=i*i
+//   console.log('i - > ', i,' sum - > ',sum);
+// }
+// console.log(sum);
+
+// 10-masala
+// let sum=0;
+
+// for(let i=1;i<=3;i++){
+//   sum+=1/i
+//   // console.log('i - > ', i,' sum - > ',sum);
+// }
+// console.log(sum);
